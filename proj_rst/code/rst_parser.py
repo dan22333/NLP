84b469214ffae7d80db28cbccbaef678e06d9f45
levyrafi18 <raffilevy@gmail.com>
2018-08-22 09:43:04 +0300

    update

---
+++ proj_rst/code/rst_parser.py
@@ -0,0 +1,137 @@
+import glob
+import random
+
+from relations_inventory import cluster_rels_list
+from binarization import Node
+from binarization import print_gold
+
+path_to_training = "dataset\\TRAINING\\*.out.edus"
+
+class Stack(object):
+	def __init__(self):
+		self._stack = []
+
+	def pop(self):
+		return self._stack.pop(-1)
+
+	def push(self, elem):
+		return self._stack.append(elem)
+
+	def size(self):
+		return len(self._stack)
+
+class Queue(object):
+	def __init__(self):
+		self._EDUS = []
+
+	@classmethod
+	def read_file(cls, filename):
+		# print("{}".format(filename))
+		queue = Queue()
+		with open(filename) as fh:
+			for line in fh:
+				line = line.strip()
+				queue._EDUS.append(line)
+			queue._EDUS[::-1]
+		return queue
+
+	def empty(self):
+		return self._EDUS == []
+
+	def pop(self):
+		return self._EDUS.pop(-1)
+
+	def len(self):
+		return len(self._EDUS)
+
+class Transition(object):
+	def __init__(self):
+		self._nuclearity = [] # either 'Nucleus' or 'Satellite' to left and right nodes
+		self._relation = '' # cluster relation
+		self._action = '' # shift or 'reduce'
+
+def parse_files(root):
+	path = root
+	path += "\\"
+	path += path_to_training
+
+	for filename in glob.glob(path):
+		queue = Queue.read_file(filename)
+		print("{}".format(filename))
+		stack = Stack()
+		root = parse(queue, stack)
+
+		predfn = "pred\\"
+		base_name = filename.split('\\')[-1]
+		base_name = base_name.split('.')[0]
+		predfn += base_name
+		with open(predfn, "w") as ofh:
+			print_gold(ofh, root, False)
+
+		goldfn = "gold\\"
+		goldfn += base_name
+
+		n1 = count_lines(predfn) 
+		n2 = count_lines(goldfn)
+		# print("{} {} {} {} equal: {}".format(predfn, n1, goldfn, n2, n1 == n2))
+		
+def count_lines(filename):
+    lines = 0
+    for line in open(filename):
+        lines += 1
+    return lines
+
+def get_transition(queue, stack):
+	transition = Transition()
+
+	if stack.size() < 2:
+		transition._action = "shift"
+	elif not queue.empty():
+		actions = ["shift", "reduce"]
+		ind = random.randint(0,1)
+		transition._action = actions[ind]
+	else:
+		transition._action = "reduce"
+		
+	if transition._action == "shift":
+		return transition
+
+	transition._relation = cluster_rels_list[random.randint(0, len(cluster_rels_list) - 1)]
+	nuclearity_options = ["Nucleus", "Satellite"]
+	transition._nuclearity.append(nuclearity_options[random.randint(0,1)])
+	transition._nuclearity.append(nuclearity_options[random.randint(0,1)])
+
+	return transition
+
+def parse(queue, stack):
+	leaf_ind = 1
+	while not queue.empty() or stack.size() != 1:
+		node = Node()
+		transition = get_transition(queue, stack)
+		if transition._action == "shift":
+			# create a leaf
+			node._text = queue.pop()
+			node._type = 'leaf'
+			node._span = [leaf_ind, leaf_ind]
+			leaf_ind += 1
+		else:
+			childs = [stack.pop(), stack.pop()]
+			if childs[0]._span[0] > childs[1]._span[1]:
+				childs = childs[::-1]
+			[l, r] = childs
+			node._childs.append(l)
+			node._childs.append(r)
+			node._relation = transition._relation
+			l._nuclearity = transition._nuclearity[0]
+			r._nuclearity = transition._nuclearity[1]
+			if queue.empty() and stack.size() == 0:
+				node._type = "Root"
+			else:
+				node._type = "span"
+			node._span = [l._span[0], r._span[1]]
+		stack.push(node)
+
+	return stack.pop()
+
+if __name__ == '__main__':
+	parse_files("..")
---
+++ proj_rst/code/training_samples.py
@@ -0,0 +1,101 @@
+from binarization import Node
+from binarization import binarize_file
+from utils import map_to_cluster
+from rst_parser import Queue
+
+import glob
+
+path_to_dis = "*.out.dis"
+
+class Operation(object):
+	def __init__(self):
+		self._state = [] # [v1, v2, v3] where v1 & v2 are the elements at the top of the stack
+		self._action = ''
+
+	def print_info(self):
+		print("operation {} {}".format(self._state, self._action))
+
+def gen_training_samples(path):
+	offset = 0
+	for fn in glob.glob(path):
+		root = binarize_file(fn)
+
+		base_name = fn.split('.')[0]
+		edus_fn = base_name
+		edus_fn += ".out.edus"
+		EDUS = Queue.read_file(edus_fn)
+
+		stack = []
+		samples = []
+		queue = []
+		for i in range(EDUS.len()):
+			queue.append(i + 1)
+		queue = queue[::-1]
+		
+		gen_training_samples_tree(root, stack, queue, samples, offset)
+		# offset += EDUS.len()
+
+		outfn = "train_samples\\"
+		outfn += base_name
+		with open(outfn, "w") as ofh:
+			for sample in samples:
+				ofh.write("{} {}\n".format(sample._state, sample._action))
+
+def gen_training_samples_tree(node, stack, queue, samples, offset):
+	# node.print_info()
+	operation = Operation()
+	if node._type == "leaf":
+		operation._action = "SHIFT"
+		operation._state = gen_state(stack, queue, offset)
+		assert(queue.pop(-1) == node._span[0])
+		stack.append(node)
+	else:
+		[l, r] = node._childs
+		gen_training_samples_tree(l, stack, queue, samples, offset)
+		gen_training_samples_tree(r, stack, queue, samples, offset)
+		if r._nuclearity == "Satellite":
+			operation._action = gen_action(node, r)
+		else:
+			operation._action = gen_action(node, l)
+	
+		assert(stack.pop(-1) == node._childs[1])
+		assert(stack.pop(-1) == node._childs[0])
+		stack.append(node)
+		operation._state = gen_state(stack, queue, offset)
+
+	if node._type != "Root":
+		samples.append(operation)
+
+def gen_action(parent, child):
+	left_right = "LEFT" if parent._childs[0] == child else "RIGHT"
+	action = "REDUCE-"
+	action += map_to_cluster(child._relation)
+	action += "-"
+	action += left_right
+	return action
+		
+def gen_state(stack, queue, offset):
+	ind1 = 0
+	ind2 = 0
+	ind3 = 0;
+	if len(queue) > 0:
+		ind3 = offset + queue[-1]
+
+	if len(stack) > 0:
+		ind1 = offset + get_nuclear_edu_ind(stack[-1])
+		if len(stack) > 1:
+			ind2 = offset + get_nuclear_edu_ind(stack[-2])
+
+	return [ind1, ind2, ind3]
+
+def get_nuclear_edu_ind(node):
+	if node._type == "leaf":
+		return node._span[0]
+	l = node._childs[0]
+	r = node._childs[1]
+	if l._nuclearity == "Nucleus":
+		return get_nuclear_edu_ind(l)
+	return get_nuclear_edu_ind(r)
+
+if __name__ == '__main__':
+	gen_training_samples(path_to_dis)
